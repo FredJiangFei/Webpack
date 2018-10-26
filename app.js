28c12762cb0dff0abadb900fbf3a34af0e0f4850
FredJiang <jiang.fei@ffcg.se>
2018-10-26 11:19:58 +0800

    babel

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-// es module
+// import 'babel-polyfill'
 import sum from './sum'
+
 console.log('sum(23, 24) =', sum(23, 24))
+
+ let fun = () => { };
+const NUM = 20;
+let arr = [1, 2, 3];
+let arrB = arr.map(i => i * 2);
+console.log('new Set(arrB):', new Set(arrB));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const path = require('path');
 
 module.exports = {
@@ -9,5 +7,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname),
         filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
     }
 };
